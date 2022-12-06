f22d81594c3ec1e7c1c13d7fd236cba8d3dbf262
Dengoan969 <dengoan969@gmail.com>
2022-12-06 10:11:25 +0500

    Beginning

---
+++ DomitoryBot/DomitoryBot/Domain/Schedule.cs
@@ -0,0 +1,79 @@
+﻿namespace DomitoryBot.Domain
+{
+    public enum WashingType
+    {
+        a,
+        b,
+        c
+    }
+
+    public class TimeInterval
+    {
+        private readonly DateTime start;
+        private readonly DateTime end;
+        public TimeInterval(DateTime start, DateTime end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+
+        public bool Contains(DateTime moment)
+        {
+            return moment >= start && moment <= end;
+        }
+    }
+
+    public class Schedule
+    {
+        IScheduleData data;
+        string[] machineNames;
+
+        public Schedule(IScheduleData data) => this.data = data;
+
+        public bool AddRecord(Guid User, string machine, DateTime date, WashingType washingType)
+        {
+            foreach(var pair in GetFreeTimes())
+            {
+                foreach(var timeInterval in pair.Value)
+                {
+                    if(timeInterval.Contains(date.AddSeconds((double)washingType)))
+                    {
+
+                    }
+                }
+            }
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRecord(Guid user)
+        {
+            data.RemoveRecord(user);
+        }
+
+        public DateTime GetRecordTime(Guid user)
+        {
+            //data.GetRecord(user).Time;
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, TimeInterval[]> GetFreeTimes()
+        {
+            
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+    public interface IScheduleData // Взять у Рината
+    {
+        bool AddRecord();
+        void RemoveRecord(Guid id);
+        void GetRecords();
+        void GetRecord();
+    }
+
+    public interface ITimeTable // Взять у Рината
+    {
+    }
+}
---
+++ DomitoryBot/DomitoryBot/Domain/SubscribitionService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomitoryBot.Domain
+{
+    public class SubscribitionService
+    {
+    }
+
+    public class Announcement
+    {
+    }
+
+}
